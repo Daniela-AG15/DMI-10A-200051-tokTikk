7d73c978b7d922d3c58a27c280a35070b8ec0da6
Daniela-AG15 <danaguilar1512@gmail.com>
2024-11-29 11:32:00 -0600

    Practica25

---
+++ toktik_200051/lib/presentation/screens/discover/discover_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:toktik/presentation/providers/discover_provider.dart';
+// import 'package:toktik/presentation/widgets/shared/video_scrollable_view.dart';
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:  Center( child: Text("DiscoverScreen") )
+    );
+  }
+}
